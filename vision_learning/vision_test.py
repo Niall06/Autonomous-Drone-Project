9632a8faee823f511a704f5db66b4105d157ffe2
Niall <bluefireapple@gmail.com>
2026-08-20 09:30:17 +0100

    continued writing vision test python script

--- vision_learning/vision_test.py
+++ vision_learning/vision_test.py
@@ -40,5 +40,41 @@ def main():
         mask = mask1 + mask2
         # This creates a boolean mask for the specified red pixels that the drone will track. 
 
-        
-    
+        # To make the image more clear morphological dilation is used
+        mask = cv2.dilate(mask, None, iterations=2)
+
+        # Find contours in the mask
+        contours, _= cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        # Next, crosshairs are drawn at the centre
+        cv2.drawMarker(frame, (centre_x, centre_y), (255, 255, 255), cv2.MARKER_CROSS, markerSize=20, thickness=2)
+
+        # The largest contor is selected, (biggest blob)
+
+        if contours:
+            largest_contour = max(contours, key=cv2.contourArea)
+
+            #Filter out small contours based on their area
+            if cv2.contourArea(largest_contour)> 500:
+                # Now draw bounding box around the largest blob
+                x, y, w, h = cv2.boundingRect(largest_contour)
+                #and find drone target position
+                
+
+                target_x = x + w //2 
+                target_y = y + h //2
+
+                # Pixel displacement error
+
+                delta_x = target_x - centre_x
+                delta_y = target_y - centre_y
+
+                cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
+                cv2.circle(frame, (target_x, target_y), 5, (0,255,0), -1)
+
+                # Line from centre to target
+                cv2.line(frame, (centre_x, centre_y), (target_x, target_y), (255, 0, 0), 2)
+                
+
+
+
